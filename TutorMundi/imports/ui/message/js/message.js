079a364f30ddad61dc5c39c430a64465bd1d6907
Fernando Henrique Guimarães <fernandohenriqueguimaraes@gmail.com>
2018-11-01 03:39:23 -0300

    Update message.js

--- TutorMundi/imports/ui/message/js/message.js
+++ TutorMundi/imports/ui/message/js/message.js
@@ -1,7 +1,7 @@
  import { Template } from 'meteor/templating';
  import '../message.html';
 
- // Responsável por calcular o tempo em que a última mensagem foi enviada
+ // Responsável por calcular o tempo em que a mensagem foi enviada
  Template.message.helpers({
 	 timeDifference(postTime) {
 			return moment(postTime).fromNow();
